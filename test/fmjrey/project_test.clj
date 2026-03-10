c8f147820fd95a084a20b897b8bce6f9b2626c32
Francois Rey <your-website@myfirstname.mylastname.name>
2026-03-10 19:10:29 +0100

    Initial commit with project generated from deps-new lib

---
+++ test/fmjrey/project_test.clj
@@ -0,0 +1,7 @@
+(ns fmjrey.project-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [fmjrey.project :as sut])) ; system under test
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
